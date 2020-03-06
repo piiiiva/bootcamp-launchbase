40e7087ae8b2947580717cca5505055cd4005299
PivaDesk <piiiiva@gmail.com>
2020-03-05 23:48:33 -0300

    CSS grid; iframe; DOM manipulate with js

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,8 +1,21 @@
 const modalOverlay = document.querySelector('.modal-overlay') ; // docmunet é como se fosse o global do meu html e o js entende que cada elemento do html é um objeto
 const cards = document.querySelectorAll('.card') // Uso querySelectorAll quando tenho diversos elementos do mesmo tipo 
+// seleciono um elemento do html mas preciso colocar ele dentro de uma constante aqui no js pra poder trabalhar de forma melhor
+// no caso de precisar fazer um for conforme abaixo 
+
 
 for (let card of cards) {
-    card.addEventListener("click", function(){
-        modalOverlay.classList.add('active')
+    card.addEventListener("click", function(){ // preciso informar como parametro o evento "click" e o que fazer quando esse evento ocorrer, a funçao!
+        const videoId = card.getAttribute("id") /* Antes de criar eu defini ela no querySelector no iframe, decidi que seria dinamico pra pegar o id e pra fazer isso 
+        tem que colocar um id para cada <div class="card" id="id do vídeo" assim, o for percorre todos os cards e pega o id do video de cada um e substitui
+        no src do iframe no modalOverlay.querySelector("iframe").src */
+        modalOverlay.classList.add('active') // essa é funçao principal dessa funçao, quando clicar eu vou adicionar a classe active na minha <div class= "modal-overlay"
+        modalOverlay.querySelector("iframe").src = `https://youtube.com/embed/${videoId}` // poderia ser document.classList.add ... mas eu já conehço o modal e o script nao precisa percorrer o html inteiro procurando o elemento que eu quero
     })
 }
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active") // precviso remover a funçao do evento anterior para voltar ao normal a página. Se nao fizer isso a <div class= "modal-overlay"> vai continuar com a class = "active" e manter as propriedades definidas no css, opacidade 1 no caso 
+    modalOverlay.querySelector("iframe").src = "" // se nao fizer isso o veio vai continuar rodando mesmo depoios de fechar a janela
+})
+
