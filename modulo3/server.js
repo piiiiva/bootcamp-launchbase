1d0494e746f1bb52f0c67da4e6e6b9cedaa6a7fd
PivaDesk <piiiiva@gmail.com>
2020-03-20 22:53:09 -0300

    setting njk files

--- modulo3/server.js
+++ modulo3/server.js
@@ -6,7 +6,7 @@ const server = express() //2
 // o express vai ficar observando essa pasta public pra servir os arquivos estáticos, onde ficara o css por exemplo
 server.use(express.static('public')) // 7 tenho que criar uma pasta public que vai conter o css dentre outras coisas
 
-server.set("view engine", "html") //5
+server.set("view engine", "njk") //5 -- 8 trocar "html" por "njk" e renomeio todos os meus arquivos html por njk ... precisa instalar o templateNujucks e habilitar o emmet do javascrip no ctrl+shit+P settings.json .. se nao habilitar nao vai funcionar o div.class por exemplo
 
 nunjucks.configure("views", { //6
     express: server
