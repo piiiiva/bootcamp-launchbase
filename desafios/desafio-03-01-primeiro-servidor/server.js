47c05f8918e336724e07c4ccbfbea71b788f3b5f
PivaDesk <piiiiva@gmail.com>
2020-03-22 01:31:26 -0300

    setting nunjucks; create a layout base page and use nunjucks to aplly this; create a not-found page

--- desafios/desafio-03-01-primeiro-servidor/server.js
+++ desafios/desafio-03-01-primeiro-servidor/server.js
@@ -5,7 +5,7 @@ const server = express()
 
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure('views', {
     express: server
@@ -15,12 +15,12 @@ server.listen(5000, function () {
     console.log("Piva server is running")
 })
 
-server.get("/", function(get, res){
-    return res.render("about")
+server.get("/", function(req, res){
+    return res.render("courses")
 })
 
-server.get("/courses", function(get, res){
-    return res.render("courses")
+server.get("/about", function(req, res){
+    return res.render("about")
 })
 
 server.use(function(req, res) {
