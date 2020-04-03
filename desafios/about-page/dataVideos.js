1dee49ec42447e94e5301bb6f73152313beb200e
PivaDesk <piiiiva@gmail.com>
2020-04-03 00:15:03 -0300

    Challenge 3-2
    Nunjucks files, dynamic data, backend/frontend communication

---
+++ desafios/about-page/dataVideos.js
@@ -0,0 +1,59 @@
+module.exports = [
+    
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e Github na prática! | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "erInHkjxkL8",
+        title: "Padronização dos commits | Diego Fernandes",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "mcd7lqpUzIA",
+        title: "Contribuições no Github! | Diego Fernandes",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS e CSS | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS grid na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável this | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "bVAhNaxBEjM",
+        title: "Debug de aplicações Node.js com VSCode | Diego Fernandes",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "pW8GKJtHSk8",
+        title: "Pré Processador SASS | Cleiton Souza",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Criando Player de Áudio com Javascript | Diego Fernandes",
+        duration: "57 min",
+        price: "Free"
+    },
+    
+    
+]
--- desafios/about-page/server.js
+++ desafios/about-page/server.js
@@ -2,26 +2,46 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const dataVideos = require('./dataVideos')
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false,
+    watch: true,
+
+    
 })
 
 server.listen(5000, function () {
     console.log("Piva server is running")
 })
 
-server.get("/", function(req, res){
-    return res.render("courses")
+server.get("/portfolio", function(req, res){
+    return res.render("portfolio", { dataVideos })
 })
 
-server.get("/about", function(req, res){
-    return res.render("about")
+server.get("/", function(req, res){
+    const about = {
+        avatar: "profilepiva.jpeg",
+        name: "Rodrigo Piva",
+        role: "Jr Web Developer",
+        description: 'In love with web developer world! A <a href="https://skylab.rocketseat.com.br/dashboard" target="blank">Rocketseat</a> student, full-stack developer, focused on learn and help you to find a way!',
+        socialMedias: [
+            {name: "Github", url: "https://github.com/piiiiva"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/rodrigo-piva-10772827/"},
+            {name: "Twitter", url: "https://twitter.com/piiiiva"},
+            {name: "Instagram", url: "https://www.instagram.com/piiiiva/"},
+
+        ]
+    }
+
+    return res.render("about", { about })
 })
+
 
 server.use(function(req, res) {
     res.status(404).render("not-found");
