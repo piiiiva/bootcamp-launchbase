8d6ca6d779b24aba23b081bea620ed047c68f951
PivaDesk <piiiiva@gmail.com>
2020-04-05 23:13:53 -0300

    Challenge 3-3
    Created Data files simulation of courses; Finished launchbase.njk layout; Withdraw modal and iframe of index.njk and all related css and  functions in scripts.js

---
+++ desafios/courses/data-courses.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        "id": "starter",
+        "name": "Starter",
+        "image": "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        "modules": "10",
+        "price": "Free",    
+    },
+
+    {
+        "id": "launchbase",
+        "name": "LauchBase",
+        "image": "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        "modules": "10",
+        "price": "900",    
+    },
+
+    {
+        "id": "gostack",
+        "name": "GoStack",
+        "image": "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        "modules": "10",
+        "price": "2900",    
+    },
+]
