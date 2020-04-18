a51073931ecbfca99e564de00b924c275654fdd7
PivaDesk <piiiiva@gmail.com>
2020-04-18 01:53:45 -0300

    Storaging data of req.body in a JSON using node fs - fs.writeFile()

--- modulo4/instructors.js
+++ modulo4/instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 // create
 exports.post = function(req, res) {
     //["avatar_url","name","birth","gender","services"]
@@ -10,9 +13,17 @@ exports.post = function(req, res) {
     
     }
 
-    return res.send(req.body)
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("write file error!!!")
+    
+        return res.redirect('instructors')
+    })
+    // return res.send(req.body)
     
 }
+
 
 // update
 
