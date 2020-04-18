7ed3ee144ff154fc92dcca37f25c172c05c79107
PivaDesk <piiiiva@gmail.com>
2020-04-18 15:27:25 -0300

    Working with dates; Destructuring req.body; Creating id with array length

--- modulo4/instructors.js
+++ modulo4/instructors.js
@@ -12,19 +12,29 @@ exports.post = function(req, res) {
             return res.send('Please, fill all fields')
     
     }
+    let {avatar_url, birth, name, services, gender} = req.body
 
-    data.instructors.push(req.body)
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1) // envolver no construtor Number para garantir que será um número sempre
+
+
+    //[{...}]
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("write file error!!!")
     
         return res.redirect('instructors')
     })
-    // return res.send(req.body)
-    
+ 
 }
-
-
-// update
-
-// delete
