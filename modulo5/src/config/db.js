90aee2d96ce60f0da660433dbab46cd5780f4989
PivaDesk <piiiiva@gmail.com>
2020-05-20 01:59:42 -0300

    Installing postgres dependency; Creating db.js, db conection
    Creating and setting new Pool in db.js

---
+++ modulo5/src/config/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg") // Não vai requisitar login e senha para todas as Queries, só conectamos uma vez
+
+module.exports = new Pool ({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'gymmanager'
+})
+
