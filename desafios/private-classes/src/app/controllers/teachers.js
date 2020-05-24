ee7d1c77ced19bcf9b6394a0e58bb973a6cd0375
PivaDesk <piiiiva@gmail.com>
2020-05-23 23:58:39 -0300

    Challenge 5-2 - Interacting with DB
    Refactoring CRUD [x]-post with db queries responsabilities in models; Creating model Teacher and create function for controller post function

--- desafios/private-classes/src/app/controllers/teachers.js
+++ desafios/private-classes/src/app/controllers/teachers.js
@@ -1,5 +1,6 @@
 const Intl = require('intl')
 const { age, graduation, date } = require('../../lib/utils')
+const Teacher = require('../models/Teacher')
 
 module.exports = {
     index(req, res) {
@@ -18,121 +19,117 @@ module.exports = {
     post(req, res) {
         const keys = Object.keys(req.body)
 
-    for (key of keys) {
-        if (req.body[key] == "")
-            return res.send('Por favor, preencha todos os campos')
-    }
+        for (key of keys) {
+            if (req.body[key] == "")
+                return res.send('Por favor, preencha todos os campos')
+        }
 
-    let { avatar_url, name, birth, education, classType, services } = req.body
+        Teacher.create(req.body, function(teacher) {
+            return res.redirect(`teachers/${teacher.id}`)
+        })
 
-    birth = Date.parse(req.body.birth)
-    const created_at = Date.now()
 
-    let id = 1
-    const lastTeacher = data.teachers[data.teachers.length - 1]
+    // let { avatar_url, name, birth, education, classType, services } = req.body
 
-    if (lastTeacher) {
-        id = lastTeacher.id + 1
-    }
+    // birth = Date.parse(req.body.birth)
+    // const created_at = Date.now()
     
 
-    data.teachers.push({
-        id,
-        avatar_url,
-        name,
-        birth,
-        education,
-        classType,
-        services,
-        created_at
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-        if (err) return res.send("Erro de escrita!!!")
-
-        return res.redirect('teachers')
+    // data.teachers.push({
+    //     id,
+    //     avatar_url,
+    //     name,
+    //     birth,
+    //     education,
+    //     classType,
+    //     services,
+    //     created_at
+    // })
         
-    })
     },
     show(req, res) {
-        const { id } = req.params
+        // const { id } = req.params
 
-        const foundTeachers = data.teachers.find(function(teacher){
-            return teacher.id == id
-        })
+        // const foundTeachers = data.teachers.find(function(teacher){
+        //     return teacher.id == id
+        // })
     
-        if (!foundTeachers) return res.send("Professor não encontrado!")
+        // if (!foundTeachers) return res.send("Professor não encontrado!")
     
-        const teacher = {
-            ...foundTeachers,
-            age: age(foundTeachers.birth),
-            education: graduation(foundTeachers.education),
-            services: foundTeachers.services.toString().split(","),
-            created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeachers.created_at),
-        }
+        // const teacher = {
+        //     ...foundTeachers,
+        //     age: age(foundTeachers.birth),
+        //     education: graduation(foundTeachers.education),
+        //     services: foundTeachers.services.toString().split(","),
+        //     created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeachers.created_at),
+        // }
     
     
-        return res.render('teachers/show', { teacher })
+        // return res.render('teachers/show', { teacher })
+        return
     },
     edit(req, res) {
-        const { id } = req.params
+        // const { id } = req.params
 
-        const foundTeachers = data.teachers.find(function(teacher){
-            return teacher.id == id
-        })
+        // const foundTeachers = data.teachers.find(function(teacher){
+        //     return teacher.id == id
+        // })
     
-        if (!foundTeachers) return res.send('Professor não encontrado')
+        // if (!foundTeachers) return res.send('Professor não encontrado')
     
-        const teacher = {
-            ...foundTeachers,
-            birth: date(foundTeachers.birth).iso
-        }
+        // const teacher = {
+        //     ...foundTeachers,
+        //     birth: date(foundTeachers.birth).iso
+        // }
     
     
-        return res.render('teachers/edit', { teacher })
+        // return res.render('teachers/edit', { teacher })
+        return
     },
     update(req, res) {
-        const id = req.params.id
-        let index = 0
+        // const id = req.params.id
+        // let index = 0
     
-        console.log(id)
-        const foundTeachers = data.teachers.find(function(teacher, foundIndex){
-            if (id == teacher.id) {
-                index = foundIndex
-                return true
-            }
-        })
+        // console.log(id)
+        // const foundTeachers = data.teachers.find(function(teacher, foundIndex){
+        //     if (id == teacher.id) {
+        //         index = foundIndex
+        //         return true
+        //     }
+        // })
     
-        if (!foundTeachers) return res.send("Professor não encontrado")
+        // if (!foundTeachers) return res.send("Professor não encontrado")
     
-        const teachers = {
-            ...foundTeachers,
-            ...req.body,
-            birth: Date.parse(req.body.birth),
-            id: Number(req.params.id)
-        }
+        // const teachers = {
+        //     ...foundTeachers,
+        //     ...req.body,
+        //     birth: Date.parse(req.body.birth),
+        //     id: Number(req.params.id)
+        // }
     
-        data.teachers[index] = teachers
+        // data.teachers[index] = teachers
         
-        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-            if (err) return res.send('Professor não encontrado!')
+        // fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        //     if (err) return res.send('Professor não encontrado!')
     
-            return res.redirect(`/teachers/${id}`)
-        })
+        //     return res.redirect(`/teachers/${id}`)
+        // })
+        return
     },
     delete(req, res) {
-        const { id } = req.body
+        // const { id } = req.body
 
-        const filteredTeachers = data.teachers.filter(function(teacher){
-            return teacher.id != id
-        })
+        // const filteredTeachers = data.teachers.filter(function(teacher){
+        //     return teacher.id != id
+        // })
     
-        data.teachers = filteredTeachers
+        // data.teachers = filteredTeachers
     
-        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-            if (err) return res.sed("Write file error!")
-        } )
+        // fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        //     if (err) return res.sed("Write file error!")
+        // } )
     
-        return res.redirect('teachers')
+        // return res.redirect('teachers')
+        return
     },
 }
---
+++ desafios/private-classes/src/app/models/Teacher.js
@@ -0,0 +1,49 @@
+const { age, graduation, date } = require('../../lib/utils')
+const db = require('../../config/db')
+
+module.exports = {
+    all(calback) {
+
+    },
+    create(data, callback) {
+        const query = `
+            INSERT INTO teachers (
+                avatar_url,
+                name,
+                birth_date,
+                education_level,
+                class_type,
+                subject_taught,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING id
+            `
+        
+        const values = [
+            data.avatar_url,
+            data.name,
+            date(data.birth_date).iso,
+            data.education_level,
+            data.class_type,
+            data.subject_taught,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results) {
+            if(err) throw `Database Error! ${err}`
+
+            callback(results.rows[0])
+        })
+    },
+    find(){
+        
+    },
+    update() {
+
+    },
+    delete() {
+
+    }
+
+
+}
