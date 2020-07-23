cb2e1df5f78456dfa8367e9547b5de6ce538a9cf
piiiiva <piiiiva@gmail.com>
2020-07-23 02:33:35 -0300

    Starting with Promises

--- modulo6/src/app/controllers/ProductController.js
+++ modulo6/src/app/controllers/ProductController.js
@@ -1,5 +1,19 @@
+const Category = require('../models/Category')
+
 module.exports = {
   create(req, res) {
-    return res.render('products/create.njk')
+    // Pegar Categorias
+    Category.all()
+    .then(function(results) { // Só vamos renderizar a página quando tivermos o resultado de Category.all()
+
+      const categories = results.rows
+
+      return res.render('products/create.njk', { categories })
+    }).catch(function(err) {
+      throw new Error(err)
+    })
+  },
+  post(req, res) {
+    // Lógica de salvar
   }
 }
