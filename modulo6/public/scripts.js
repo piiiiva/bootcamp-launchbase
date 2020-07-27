0cd8bed80bd422e88221f9f76805fbf039b94094
piiiiva <piiiiva@gmail.com>
2020-07-27 01:01:27 -0300

    Creating a field mask to handle with inputs

--- modulo6/public/scripts.js
+++ modulo6/public/scripts.js
@@ -1,16 +1,15 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function(e) {
-  
-  setTimeout(function() {
-    let { value } = e.target
-  
+const Mask = {
+  apply(input, func) {
+    setTimeout(function() {
+      input.value = Mask[func](input.value)
+    }, 1)
+  },
+  formatBRL(value) {
     value = value.replace(/\D/g, "") // pega tudo o que não é Dígito e troca por vazio
     
-    value = new Intl.NumberFormat('pt-BR', {
-      style: 'currency', // 1.000,00 que é o do Brasil pt-BR
-      currency: 'BRL' // R$
+    return value = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
     }).format(value/100)
-
-    e.target.value = value
-  }, 1)
-})
+  }
+}
