5b78df1bb58477615d2ca47964d68dc9fed35bab
piiiiva <piiiiva@gmail.com>
2020-09-30 23:43:04 -0300

    feature update products

--- modulo6/src/app/controllers/ProductController.js
+++ modulo6/src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 const { formatPrice } = require('../../lib/utils')
+const { put } = require('../../routes')
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
@@ -38,12 +39,36 @@ module.exports = {
 
     if(!product) return res.send("Produto não encontrado")
 
-    product.oldPrice = formatPrice(product.oldPrice)
+    product.old_price = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
     results = await Category.all()
     const categories = results.rows
 
     return res.render('products/edit.njk', { product, categories })
-  }
+  },
+
+  async put(req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send('Por favor, preencha todos os campos.')
+      }
+    }
+    
+    req.body.price = req.body.price.replace(/\D/g, "") // onde não for dígito troca por vazio
+
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id)
+      req.body.old_price = oldProduct.rows[0].price
+    }
+
+    await Product.update(req.body)
+
+    return res.redirect(`/products/${req.body.id}/edit`)
+
+
+
+  },
 }
--- modulo6/src/routes.js
+++ modulo6/src/routes.js
@@ -9,6 +9,7 @@ routes.get('/', function (req, res) {
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
 
 // Alias - atalhos
 routes.get('/ads/create', function (req, res) { // máscara de redirecionamento
