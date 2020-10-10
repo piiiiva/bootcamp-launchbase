ade6f3308502c4e5b689f2df1acfa72f907023a6
piiiiva <piiiiva@gmail.com>
2020-10-10 01:11:25 -0300

    Using callbacks

--- desafios/desafio6/funcoesAssincronas.js
+++ desafios/desafio6/funcoesAssincronas.js
@@ -1,18 +1,28 @@
-function printDouble(number) {
+function printDouble(number, callback) {
   setTimeout(
     () => {
-      console.log(number * 2)
+      const double = number * 2
+      console.log(double)
+      callback(double)
     },
     Math.floor(Math.random() * 180) + 1
   )
 }
 
 function printAll() {
-  printDouble(5)
-  printDouble(15)
-  printDouble(22)
-  printDouble(1)
-  printDouble(89)
+  printDouble(5, (double) => {
+    printDouble(15, (double) => {
+      printDouble(12, (double) => {
+        printDouble(1, (double) => {
+          printDouble(89, () => {})
+        })
+      })
+    })
+  })
+  // printDouble(15)
+  // printDouble(22)
+  // printDouble(1)
+  // printDouble(89)
 }
 
 printAll()
